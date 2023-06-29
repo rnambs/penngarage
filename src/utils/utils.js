2f6b71237f5994104e21f5debfd0abd36d4a3033
Soyoon Park <112910438+salmary@users.noreply.github.com>
2023-06-29 13:33:13 -0400

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,5 @@
 import axios from 'axios';
 
-export const config = {
-  bucketName: 'penngaragebucket',
-  // dirName: 'media', /* optional */
-  region: 'us-east-2',
-  accessKeyId: 'AKIAZNTSTW7SLQBVD54Q',
-  secretAccessKey: 'g7HSMVNMCBNzpvAurMHPSHBfxcGgLhFU4VaaUKmG',
-  // s3Url: 'https:/your-custom-s3-url.com/', /* optional */
-};
-
 export const ROOT_URL = 'http://localhost:3000';
 // update the URL to be what Heroku gives us
 export const JSON_SERVER_URL = '';
